c58128a6576dcc156d697831043c582e7b2dc953
Arpan NLN <arpannln@ucla.edu>
2020-10-14 20:02:58 -0700

    fix routing issue

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,4 @@
-const HOME = '/';
-const BROWSE = '/browse';
-const SIGN_UP = '/signup';
-const SIGN_IN = '/signin';
+export const HOME = '/';
+export const BROWSE = '/browse';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
